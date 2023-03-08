7b51820d8b9978d20826329b3606dea207a33f08
giipsy0 <filyarudnev@mail.ru>
2023-03-08 15:54:33 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { applyMiddleware, compose, legacy_createStore } from 'redux'
+import reduxThunk from 'redux-thunk'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { fetchTicketsIfNeeded } from './actions'
+import App from './components/App/App'
+import './index.css'
+import rootReducer from './reducers'
+import AviasalesService from './services/AviasalesService'
+
+export const aviasales = new AviasalesService()
+
+const actionSanitizer = (action) =>
+  action.type === 'FILE_DOWNLOAD_SUCCESS' && action.data ? { ...action, data: '<<LONG_BLOB>>' } : action
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        actionSanitizer,
+        stateSanitizer: (state) => (state.data ? { ...state, data: '<<LONG_BLOB>>' } : state),
+      })
+    : compose
+
+const loggerMiddleware = () => (next) => (action) => {
+  const result = next(action)
+  return result
+}
+
+const store = legacy_createStore(rootReducer, composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk)))
+store.dispatch(fetchTicketsIfNeeded())
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+)
---
+++ src/reducers/CounterReducer.js
@@ -0,0 +1,24 @@
+/* eslint-disable default-param-last */
+const initialState = {
+    status: '',
+    progress: 0,
+};
+
+const CounterReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "REQUEST_TICKETS":
+        return {
+          status: 'loading',
+          progress: Math.min(state.progress + 10, 99),
+        };
+      case "TICKETS_RECIEVED":
+        return {
+          status: 'loaded',
+          progress: 100,
+        };
+      default:
+        return state;
+    }
+};
+
+export default CounterReducer;
